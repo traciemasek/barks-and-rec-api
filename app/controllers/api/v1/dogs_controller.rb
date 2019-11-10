df73a9057c42d5d2919e1cbe974ff4d00e8e786d
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-11-09 19:16:02 -0500

    update and create for dog

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -8,7 +8,17 @@ class Api::V1::DogsController < ApplicationController
     # byebug
     dog = Dog.create(dog_params)
     if dog.valid?
-      render json: :dog, status: :created
+      render json: dog, status: :created
+    else
+      render json: {errors: dog.errors.full_messages}, status: :not_acceptable
+    end
+  end
+
+  def update
+    # byebug
+    dog = Dog.find_by(id: params[:id])
+    if dog.update(dog_params)
+      render json: dog, status: :ok
     else
       render json: {errors: dog.errors.full_messages}, status: :not_acceptable
     end
@@ -17,6 +27,6 @@ class Api::V1::DogsController < ApplicationController
   private
   def dog_params
     params.require(:dog).permit(:name, :breed, :img1, :img2, :img3,
-    :size, :age, :adopted, :sex, :color, :about, :houseTrained, :health, :goodHome, :badHome)
+    :size, :age, :adopted, :sex, :color, :about, :houseTrained, :health, :goodHome, :badHome, images: [])
   end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,5 @@
 class Dog < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :adopters, through: :favorites
+  has_many_attached :images
 end
